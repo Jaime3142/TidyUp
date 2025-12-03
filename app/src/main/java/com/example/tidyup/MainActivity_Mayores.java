deef6f53e5c55d0c7b5b5a5986426f7f42a46c4d
champyto <ivangarcia200405@gmail.com>
2025-12-03 14:44:10 +0100

    Activities principales creados

---
+++ app/src/main/java/com/example/tidyup/MainActivity_Mayores.java
@@ -0,0 +1,14 @@
+package com.example.tidyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_Mayores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_mayores);
+    }
+}

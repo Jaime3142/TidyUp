ea50b88fce66d06c17f4628220c18111fa9948d8
Jaime <jnc0021@alu.medac.es>
2026-01-27 11:01:35 +0100

    Ultimo fragment terminado, funciona la pantalla de primera vista y ajustado y tocado el manifest y codigo de java

--- app/src/main/java/com/example/tidyup/MainActivity_Mayores.java
+++ app/src/main/java/com/example/tidyup/MainActivity_Mayores.java
@@ -1,14 +1,31 @@
 package com.example.tidyup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity_Mayores extends AppCompatActivity {
+
+    private FragmentContainerView fg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_mayores);
+    fg = findViewById(R.id.fragmentContainerView);
+
+
+    }
+
+    public void reemplazarContainer(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView,fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/tidyup/Recordatorio1.java
+++ app/src/main/java/com/example/tidyup/Recordatorio1.java
@@ -2,11 +2,14 @@ package com.example.tidyup;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,19 +26,14 @@ public class Recordatorio1 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button bRecordatorio;
+    private Button bCalendario;
 
     public Recordatorio1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Recordatorio1.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static Recordatorio1 newInstance(String param1, String param2) {
         Recordatorio1 fragment = new Recordatorio1();
@@ -52,7 +50,9 @@ public class Recordatorio1 extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+
     }
 
     @Override
@@ -60,5 +60,12 @@ public class Recordatorio1 extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_recordatorio1, container, false);
+
+    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    /*Para que cuando se cree el primer activity, salga la primera pestaña*/
+        bRecordatorio = view.findViewById(R.id.brec);
+        bCalendario   = view.findViewById(R.id.bcal);
     }
 }

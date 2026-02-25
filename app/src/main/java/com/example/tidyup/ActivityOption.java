90de971c3d2b578d5b69979907d656f60c4c862b
Jaime <jnc0021@alu.medac.es>
2026-02-25 13:25:51 +0100

    cambios de errores de codigo jaime

--- app/src/main/java/com/example/tidyup/ActivityOption.java
+++ app/src/main/java/com/example/tidyup/ActivityOption.java
@@ -12,33 +12,5 @@ import android.widget.ImageButton;
 
 public class ActivityOption extends AppCompatActivity {
 
-    private FragmentContainerView fg;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_mayores);
-        fg = findViewById(R.id.fragmentContainerView);
-
-        // --- IMPLEMENTACIÓN DEL BOTÓN HOME ---
-        // 1. Buscamos el botón de la casita (Home)
-        ImageButton btnHome = findViewById(R.id.imageButton4);
-
-        // 2. Al hacer clic, usamos TU función reemplazarContainer
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Llamamos a tu método con el fragmento de los recordatorios
-                reemplazarContainer(new Recordatorio1());
-            }
-        });
-        // ---------------------------------------
-    }
-
-    public void reemplazarContainer(Fragment fragment){
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
-        fragmentTransaction.commit();
-    }
 }
---
+++ app/src/main/java/com/example/tidyup/MainActivity_Mayores.java
@@ -0,0 +1,44 @@
+package com.example.tidyup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity_Mayores extends AppCompatActivity {
+
+    private FragmentContainerView fg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_mayores);
+        fg = findViewById(R.id.fragmentContainerView);
+
+        // --- IMPLEMENTACIÓN DEL BOTÓN HOME ---
+        // 1. Buscamos el botón de la casita (Home)
+        ImageButton btnHome = findViewById(R.id.imageButton4);
+
+        // 2. Al hacer clic, usamos TU función reemplazarContainer
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llamamos a tu método con el fragmento de los recordatorios
+                reemplazarContainer(new Recordatorio1());
+            }
+        });
+        // ---------------------------------------
+    }
+
+    public void reemplazarContainer(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView, fragment);
+        fragmentTransaction.commit();
+    }
+}

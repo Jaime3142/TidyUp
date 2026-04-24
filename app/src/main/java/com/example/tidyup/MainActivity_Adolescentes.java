468f1a8348ffdac5a61818a2dac615fd62d4424c
IvanGarcia-Medac <igf0015@alu.medac.es>
2026-04-24 10:48:53 +0200

    fixes y arreglos de adolescentes para fluidez y comodidad

--- app/src/main/java/com/example/tidyup/MainActivity_Adolescentes.java
+++ app/src/main/java/com/example/tidyup/MainActivity_Adolescentes.java
@@ -1,25 +1,31 @@
 package com.example.tidyup;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity_Adolescentes extends AppCompatActivity {
 
     public FragmentContainerView fragmentContiner;
+    public ImageButton botonHome;
+
+    public ImageButton botonAjustes;
 
     public ImageButton botonNotificaciones;
     public ImageButton botonRecompensas;
     public ImageButton botonTareas;
 
     public ImageButton botonGrupos;
+
+    public TextView nombreUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,50 @@ public class MainActivity_Adolescentes extends AppCompatActivity {
         botonRecompensas=findViewById(R.id.botonRecompensas);
         botonTareas=findViewById(R.id.botonTareas);
         botonGrupos=findViewById(R.id.botonGrupos);
+        botonAjustes=findViewById(R.id.botonAjustes);
+        botonHome=findViewById(R.id.botonInicio);
+        nombreUsuario=findViewById(R.id.NombreUsuario);
+
+        FirebaseManager firebaseMethods = new FirebaseManager();
+
+        // Llamamos al método y le decimos qué hacer cuando reciba el nombre
+        // 3. Llamada corregida (sin el "new" delante del método)
+        firebaseMethods.obtenerNombreDesdeFirestore(new FirebaseManager.OnNombreRecuperadoListener() {
+            @Override
+            public void onNombreRecuperado(String nombre) {
+                // Ahora ya no debería salir en rojo
+                nombreUsuario.setText(nombre);
+            }
+        });
+
+
+        if (savedInstanceState == null) {
+            reemplazarFragment(new Calendario());
+        }
+
+
+        botonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reemplazarFragment(new Calendario());
+            }
+        });
+
+        botonGrupos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reemplazarFragment(new FragmentGroups());
+            }
+        });
+
+        botonAjustes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reemplazarFragment(new FragmentConfiguration());
+            }
+        });
+
+
 
         // paso de pantalla de notificaciones
         botonNotificaciones.setOnClickListener(new View.OnClickListener() {
@@ -55,6 +105,8 @@ public class MainActivity_Adolescentes extends AppCompatActivity {
                 reemplazarFragment(new fragment_recompensas());
             }
         });
+
+
 
 
 
